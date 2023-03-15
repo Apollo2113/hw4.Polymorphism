295befa6de8fbc36550834bcbc8ed4d466e570c4
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-15 15:45:56 +0200

    added class Square

---
+++ src/Square.java
@@ -0,0 +1,13 @@
+public class Square implements GeometricFigure {
+    private double width;
+    private double height;
+
+    public Square(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
