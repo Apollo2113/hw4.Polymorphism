4184c00d0070073954cdbafbda4a381392b39cb2
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-23 21:46:00 +0200

    added new children of the Participant

---
+++ src/Members/Human.java
@@ -0,0 +1,16 @@
+package Members;
+
+public class Human extends Participant {
+
+    public Human(String name, int maxRunDistance, int maxJumpHeight) {
+        super(name, maxRunDistance, maxJumpHeight);
+    }
+    public void run() {
+        System.out.println("Human " + name + " is running " + maxRunDistance + " meters.");
+    }
+
+    public void jump() {
+        System.out.println("Human " + name + " is jumping " + maxJumpHeight + " meters.");
+    }
+}
+
---
+++ src/Members/Robot.java
@@ -0,0 +1,13 @@
+package Members;
+
+public class Robot extends Participant {
+    public Robot(String name, int maxRunDistance, int maxJumpHeight) {
+        super(name, maxRunDistance, maxJumpHeight);
+    }
+    public void run() {
+        System.out.println("Robot " + name + " is running " + maxRunDistance + " meters.");
+    }
+    public void jump() {
+        System.out.println("Robot " + name + " is jumping " + maxJumpHeight + " meters.");
+    }
+}
